f764ba5acc9ba29a709783cea646468df84d37da
meganlynn012 <megshaw@byui.edu>
2020-09-19 15:37:06 -0600

    Home Draft

---
+++ meganPortal.js
@@ -0,0 +1,7 @@
+let dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+let year = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = year;
+
+document.getElementById("update").innerHTML = document.lastModified;
