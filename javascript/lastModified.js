22c7fe73044338b66b2694db8c87feaec59093ef
meganlynn012 <megshaw@byui.edu>
2020-10-08 23:53:42 -0600

    final

---
+++ javascript/lastModified.js
@@ -0,0 +1,3 @@
+document.getElementById('lastModified').innerHTML = "Last Updated: " + document.lastModified;
+
+document.getElementById('currentYear').innerHTML = "&copy; " + new Date().getFullYear();
--- javascript/meganPortal.js
+++ javascript/meganPortal.js
@@ -1,7 +1,24 @@
-let dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+/*let dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
+let now = new Date(document.lastModified);
+let thisMonth = months[now.getMonth()];
+let day = now.getDate();
 let year = new Date().getFullYear();
-document.getElementById("currentYear").innerHTML = year;
+let update = day.toString() + " " + thisMonth + " " + year.toString();
+document.getElementById("update").innerHTML = update;
 
-document.getElementById("update").innerHTML = document.lastModified;
+document.getElementById("currentYear").innerHTML = year;*/
+
+window.onload = updateBox;
+
+function updateBox(){
+    let lastUpdate = "Last Updated: " + document.lastModified;
+    let date = new Date().getFullYear();
+
+    document.getElementById('year').innerHTML = date;
+    document.getElementById('update').innerHTML = lastUpdate;
+}
+
+
+
