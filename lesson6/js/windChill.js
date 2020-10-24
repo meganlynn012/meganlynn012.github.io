cbf296a0d0047bfd0f896c8ea4e20b0c44d5f48e
meganlynn012 <megshaw@byui.edu>
2020-10-24 14:21:33 -0600

    w6 final

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,11 +1,16 @@
+//pull the temp and wind speed from the html
 let t = document.getElementById('high').innerHTML;
 let s = document.getElementById('speed').innerHTML;
+
+//windchill formula
 let chill = 35.74 + (.6215 * t) - (35.75 * (s ** .16)) + (.4275 * t * (s ** .16));
 
+//round the result to the nearest whole number
 let digits = 0;
 let rounder = Math.pow(10, digits);
 chill = Math.round(chill * rounder) / rounder;
 
+//only display the result if the temp and windspeed meet requirements
 if (t <= 50 && s > 3){
     document.getElementById("windChill").innerHTML = chill;
 }
