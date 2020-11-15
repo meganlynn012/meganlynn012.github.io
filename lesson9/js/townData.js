99bbf0a6f371e6c4042213abed8f16d2e54bd383
meganlynn012 <megshaw@byui.edu>
2020-11-14 23:19:00 -0700

    lesson9

---
+++ lesson9/js/townData.js
@@ -0,0 +1,46 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+  const towns = jsonObject['towns'];
+  let three = towns.filter(town => (town.name == "Preston" || town.name == "Fish Haven" || town.name == "Soda Springs"));
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+ 
+    three.forEach(town =>{
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let p4 = document.createElement('p');
+    let image = document.createElement('img');    
+
+h2.innerHTML = `${town.name}`;
+p1.innerHTML = `${town.motto}`;
+p2.innerHTML = `Year Founded: ${town.yearFounded}`;
+p3.innerHTML = `Population: ${town.currentPopulation}`;
+p4.innerHTML = `Annual Rainfall: ${town.averageRainfall}`;
+if (town.name == "Preston"){
+image.setAttribute('src', 'images/preston-barn300.jpg')}
+else if (town.name == "Soda Springs"){
+    image.setAttribute('src', 'images/sodasprings-barn300.jpg')
+}
+else {
+    image.setAttribute('src', 'images/fishhaven-barn300.jpg')
+};
+image.setAttribute('alt', 'Town image');
+
+card.appendChild(h2);
+card.appendChild(p1);
+card.appendChild(p2);
+card.appendChild(p3);
+card.appendChild(p4);
+card.appendChild(image);
+
+
+document.querySelector('div.town-info').appendChild(card);
+  });
+});
